70c32fc7294ad05f4be032d69ce470404720c701
Oscar M <48070861+Oscarmendez825@users.noreply.github.com>
2020-06-29 18:57:36 -0600

    V0.2
    Se agregó la lista enlazada, la clase empresa y la clase receta

---
+++ src/main/java/com/Empresas/Empresa.java
@@ -0,0 +1,36 @@
+package com.Empresas;
+
+public class Empresa {
+	private String nombre;
+	//LOGO
+	private String contacto;
+	private String horario;
+	//Direccion
+	
+	
+	
+	public Empresa() {
+		
+		
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getContacto() {
+		return contacto;
+	}
+	public void setContacto(String contacto) {
+		this.contacto = contacto;
+	}
+	public String getHorario() {
+		return horario;
+	}
+	public void setHorario(String horario) {
+		this.horario = horario;
+	}
+	
+	
+}
